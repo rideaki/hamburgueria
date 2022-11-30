52d243f94a5e2a4dc01fc928bd3cf9d5c196c592
rideaki <reginaldoime@gmail.com>
2022-11-30 19:46:03 -0300

    Update RequestController.java

--- src/main/java/com/hamburgueria/controller/RequestController.java
+++ src/main/java/com/hamburgueria/controller/RequestController.java
@@ -3,6 +3,8 @@ package com.hamburgueria.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hamburgueria.dao.ProductRepository;
 import com.hamburgueria.dao.RequestRepository;
 import com.hamburgueria.model.Request;
 
@@ -21,9 +24,13 @@ import com.hamburgueria.model.Request;
 @RequestMapping({"hamburgueria/requests"})
 public class RequestController {
 
+	Logger logger = LoggerFactory.getLogger(RequestController.class);
 	
 	@Autowired
 	private RequestRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@GetMapping
 	public List<Request> findAll(){
@@ -37,11 +44,41 @@ public class RequestController {
 
 	@PostMapping
 	public Request create(@RequestBody Request request){
+		if (request == null || request.getProducts() == null || request.getProducts().isEmpty()) {
+			logger.warn("PEDIDO SEM PRODUTO!");
+			return repository.save(request);
+		}
+		request.getProducts().forEach(product -> {
+			if (product == null) {
+				logger.error("PEDIDO COM PRODUTO INVÁLIDO!!!!!!!!!!");
+				return;
+			} else {
+				if(!productRepository.existsById(product.getId())) {
+					logger.error("PRODUTO NÃO ENCONTRADO! É NECESSÁRIO CADASTRAR O PRODUTO ANTES DE FAZER O PEDIDO!!!!!!!!!");
+					return;
+				}
+			}
+		});
 		return repository.save(request);
 	}
 
 	@PutMapping
 	public Request update(@RequestBody Request request){
+		if (request == null || request.getProducts() == null || request.getProducts().isEmpty()) {
+			logger.warn("PEDIDO SEM PRODUTO!");
+			return repository.save(request);
+		}
+		request.getProducts().forEach(product -> {
+			if (product == null) {
+				logger.error("PEDIDO COM PRODUTO INVÁLIDO!!!!!!!!!!");
+				return;
+			} else {
+				if(!productRepository.existsById(product.getId())) {
+					logger.error("PRODUTO NÃO ENCONTRADO! É NECESSÁRIO CADASTRAR O PRODUTO ANTES DE FAZER O PEDIDO!!!!!!!!!");
+					return;
+				}
+			}
+		});
 		return repository.save(request);
 	}	
 
