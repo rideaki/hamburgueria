44ed455eebd420c5357be98c0230b8d2cf27fb8b
rideaki <reginaldoime@gmail.com>
2022-11-30 15:05:25 -0300

    Implementação do Client e Address

---
+++ src/main/java/com/hamburgueria/model/Address.java
@@ -0,0 +1,105 @@
+package com.hamburgueria.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column
+	private String street;
+
+	@Column
+	private String number;
+	
+	@Column
+	private String complement;
+	
+	@Column
+	private String district;
+	
+	@Column
+	private String federalUnit;
+	
+	@Column
+	private String city;
+	
+	@Column
+	private String reference;
+
+	
+	/**
+	 *  Methods
+	 */
+	long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getComplement() {
+		return complement;
+	}
+
+	public void setComplement(String complement) {
+		this.complement = complement;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getFederalUnit() {
+		return federalUnit;
+	}
+
+	public void setFederalUnit(String federalUnit) {
+		this.federalUnit = federalUnit;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+}
---
+++ src/main/java/com/hamburgueria/model/Client.java
@@ -0,0 +1,74 @@
+package com.hamburgueria.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column
+	private String tel;
+	
+	@Column
+	private String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Address address;
+
+	
+	/**
+	 *  Methods
+	 */
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
